0573f501f9489317d755a083ac2eceb03921aa64
Bailey <erringtonbusiness@gmail.com>
2021-02-10 09:37:59 +0000

    Begin enum tags for hadesCards

---
+++ src/main/java/Hades/enums/hadesCards.java
@@ -0,0 +1,9 @@
+package Hades.enums;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class hadesCards {
+    @SpireEnum
+    public static AbstractCard.CardTags HERMES;
+}
