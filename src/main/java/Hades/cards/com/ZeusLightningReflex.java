e8e4df8a66d1a17a12cb60d73d1a4731a13868f9
Bailey <erringtonbusiness@gmail.com>
2021-02-11 20:28:31 +0000

    NC: ZeusLightningReflex

--- src/main/java/Hades/cards/com/ZeusLightningReflex.java
+++ src/main/java/Hades/cards/com/ZeusLightningReflex.java
@@ -3,7 +3,6 @@ package Hades.cards.com;
 import Hades.cards.abs.abs_hades_card;
 import Hades.enums.hadesCards;
 import Hades.util.CardInfo;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -11,23 +10,23 @@ import static Hades.Hades.makeID;
 import static Hades.util.actionShortcuts.doDef;
 import static Hades.util.actionShortcuts.doDmg;
 
-public class AthenaHolyShield extends abs_hades_card {
+public class ZeusLightningReflex extends abs_hades_card {
     private final static CardInfo cardInfo = new CardInfo(
-            AthenaHolyShield.class.getSimpleName(),
-            COSTS[3],
+            ZeusLightningReflex.class.getSimpleName(),
+            COSTS[1],
             CardType.ATTACK,
             CardTarget.ENEMY
     );
     public static final String ID = makeID(cardInfo.cardName);
-    private static final int DAMAGE = 15;
-    private static final int BLOCK = 12;
-    private static final int UPG_COST = 2;
-    public AthenaHolyShield() {
+    private static final int DAMAGE = 5;
+    private static final int BLOCK = 5;
+    private static final int UPG_COST = 0;
+    public ZeusLightningReflex() {
         super(cardInfo, false);
         setDamage(DAMAGE);
         setBlock(BLOCK);
         setCostUpgrade(UPG_COST);
-        this.tags.add(hadesCards.ATHENA);
+        this.tags.add(hadesCards.ZEUS);
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
